fead1c0f82bf23ab8a5b095fdce07c3a575b496b
vepifanio <vepifanio1@gmail.com>
2024-01-30 17:57:23 -0300

    feat: Certificate entity

--- src/certificates/entities/certificate.entity.ts
+++ src/certificates/entities/certificate.entity.ts
@@ -1,1 +1,31 @@
-export class Certificate {}
+export class Certificate {
+  id: string;
+  title: string;
+  content: string;
+  initialDate: Date;
+  endDate?: Date;
+  hours: number;
+  createdAt: Date;
+  updatedAt: Date;
+
+  resolveContent(
+    contentVariables: { [key: string]: string | number } = {},
+  ): string {
+    const contentTextResolved = Object.keys(contentVariables).reduce(
+      (text, key) => {
+        return text.replace(`{{${key}}}`, String(contentVariables[key]));
+      },
+      this.content,
+    );
+
+    const remainingVariables = contentTextResolved.match(/{{(\w+)}}/gim);
+
+    if (remainingVariables) {
+      const errorText = `Some required content variables are undefined: ${remainingVariables.join(', ')}`;
+
+      throw new Error(errorText);
+    }
+
+    return contentTextResolved;
+  }
+}
