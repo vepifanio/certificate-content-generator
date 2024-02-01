392d1f86c9bc8808e5d82c484822ea537d766199
vepifanio <vepifanio1@gmail.com>
2024-02-01 08:29:44 -0300

    test: add tests to the document class

---
+++ src/certificates/entities/document.entity.spec.ts
@@ -0,0 +1,16 @@
+import { Document } from './document.entity';
+
+describe('Document', () => {
+  it('should be able to create a document with sanitized identifier', () => {
+    const document = new Document('612.659.213-18');
+    expect(document.identifier).toBe('61265921318');
+  });
+
+  it('should not be able to create a document whit less than 11 numeric digits', () => {
+    expect(() => new Document('1111111111')).toThrow();
+  });
+
+  it('should not be able to create a document with not but only numeric digits', () => {
+    expect(() => new Document('1111111111a')).toThrow();
+  });
+});
