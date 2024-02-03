0bab6a2fe904d5596abe0f75d877c3a9429e8cee
vepifanio <vepifanio1@gmail.com>
2024-02-03 18:58:09 -0300

    feat: add create and generate methods to certificate service

--- src/certificates/certificates.service.ts
+++ src/certificates/certificates.service.ts
@@ -1,22 +1,53 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCertificateDto } from './dto/create-certificate.dto';
 import { UpdateCertificateDto } from './dto/update-certificate.dto';
+import { Certificate } from './entities/certificate.entity';
+import { CertificatesRepository } from './repositories/CertificatesRepository';
+
+interface GenerateContentVariables {
+  [key: string]: string;
+}
 
 @Injectable()
 export class CertificatesService {
-  create(createCertificateDto: CreateCertificateDto) {
-    return 'This action adds a new certificate';
+  constructor(private certificatesRepository: CertificatesRepository) {}
+
+  async create(createCertificateDto: CreateCertificateDto) {
+    const { title, content, hours, initialDate, endDate } =
+      createCertificateDto;
+    const certificate = Certificate.create(
+      title,
+      content,
+      initialDate,
+      endDate,
+      hours,
+    );
+
+    await this.certificatesRepository.create(certificate);
+    return certificate;
   }
 
   findAll() {
     return `This action returns all certificates`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} certificate`;
   }
 
-  update(id: number, updateCertificateDto: UpdateCertificateDto) {
+  async generate(id: string, variables: GenerateContentVariables) {
+    const certificate = await this.certificatesRepository.findById(id);
+
+    if (!certificate) {
+      throw new Error('Certificate not found.');
+    }
+
+    const generatedContent = certificate.resolveContent(variables);
+
+    return generatedContent;
+  }
+
+  update(id: string, updateCertificateDto: UpdateCertificateDto) {
     return `This action updates a #${id} certificate`;
   }
 
