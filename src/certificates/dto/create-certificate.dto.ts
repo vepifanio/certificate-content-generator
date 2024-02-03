6c60f60725c0bc722f2f6ff44ffe3a42ecd87d16
vepifanio <vepifanio1@gmail.com>
2024-02-03 18:58:50 -0300

    feat: add create certificate DTO

--- src/certificates/dto/create-certificate.dto.ts
+++ src/certificates/dto/create-certificate.dto.ts
@@ -1,1 +1,21 @@
-export class CreateCertificateDto {}
+export class CreateCertificateDto {
+  readonly title: string;
+  readonly content: string;
+  readonly initialDate: Date;
+  readonly endDate?: Date;
+  readonly hours: number;
+
+  constructor({
+    title,
+    content,
+    hours,
+    initialDate,
+    endDate,
+  }: CreateCertificateDto) {
+    this.title = title;
+    this.content = content;
+    this.initialDate = initialDate;
+    this.endDate = endDate;
+    this.hours = hours;
+  }
+}
