c680a77953d2d1b53ee0500f5603a20d8a7c51ee
vepifanio <vepifanio1@gmail.com>
2024-02-04 19:24:53 -0300

    refactor: change db schema

---
+++ prisma/migrations/20240204222306_add_updated_at_constraint/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "certificates" ALTER COLUMN "updatedAt" DROP DEFAULT;
---
+++ prisma/migrations/20240204222428_remove_certificate_created_at_and_updated_at_not_null_constraints/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "certificates" ALTER COLUMN "createdAt" DROP NOT NULL,
+ALTER COLUMN "updatedAt" DROP NOT NULL;
