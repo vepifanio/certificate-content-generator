edc7fb142e8933338481ee5f3f2475fcdc94d18f
vepifanio <vepifanio1@gmail.com>
2024-02-04 15:29:22 -0300

    feat: add documents field transformation to certificate mapper logic

--- src/certificates/mappers/CertificateMapper.ts
+++ src/certificates/mappers/CertificateMapper.ts
@@ -1,23 +1,43 @@
 import { Certificate } from '../entities/certificate.entity';
-import { Certificate as PrismaCertificate } from '@prisma/client';
+import {
+  Certificate as PrismaCertificate,
+  DocumentsOnCertificates,
+} from '@prisma/client';
+import { Document } from '../entities/document.entity';
 
 export class CertificateMapper {
-  static toPrisma(certificate: Certificate): PrismaCertificate {
+  static toPrisma(certificate: Certificate): {
+    certificate: PrismaCertificate;
+    documents: DocumentsOnCertificates[];
+  } {
+    const documents: DocumentsOnCertificates[] = certificate
+      .getDocuments()
+      .map((document) => ({
+        certificateId: certificate.getId(),
+        documentIdentifier: document.getIdentifier(),
+      }));
+
     return {
-      id: certificate.getId(),
-      title: certificate.getTitle(),
-      content: certificate.getContent(),
-      initialDate: new Date(certificate.getInitialDate()),
-      endDate: certificate.getEndDate()
-        ? new Date(certificate.getEndDate())
-        : undefined,
-      hours: certificate.getHours(),
-      createdAt: certificate.getCreatedAt(),
-      updatedAt: certificate.getUpdatedAt(),
+      certificate: {
+        id: certificate.getId(),
+        title: certificate.getTitle(),
+        content: certificate.getContent(),
+        initialDate: new Date(certificate.getInitialDate()),
+        endDate: certificate.getEndDate()
+          ? new Date(certificate.getEndDate())
+          : undefined,
+        hours: certificate.getHours(),
+        createdAt: certificate.getCreatedAt(),
+        updatedAt: certificate.getUpdatedAt(),
+      },
+      documents,
     };
   }
 
-  static toDomain(prismaCertificate: PrismaCertificate): Certificate {
+  static toDomain(
+    prismaCertificate: PrismaCertificate,
+    documents: DocumentsOnCertificates[],
+  ): Certificate {
     const {
       id,
       title,
@@ -28,6 +48,11 @@ export class CertificateMapper {
       createdAt,
       updatedAt,
     } = prismaCertificate;
+
+    const certificateDocuments = documents.map(
+      (document) => new Document(document.documentIdentifier),
+    );
+
     return new Certificate(
       id,
       title,
@@ -35,6 +60,7 @@ export class CertificateMapper {
       initialDate,
       endDate,
       hours,
+      certificateDocuments,
       createdAt,
       updatedAt,
     );
