b8a72763ada884b1a70e561b0502c94491199381
vepifanio <vepifanio1@gmail.com>
2024-02-23 21:41:25 -0300

    fix: add prisma module in users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { FindUserByEmailService } from './services/find-user-by-email.service';
+import { PrismaModule } from 'src/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [FindUserByEmailService],
   exports: [FindUserByEmailService],
 })
