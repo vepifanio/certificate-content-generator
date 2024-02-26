65b467a588f2da3d6c0c08e2f17a8504b44f3095
vepifanio <vepifanio1@gmail.com>
2024-02-26 08:42:46 -0300

    feat: add jwt auth guard to create users route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateUserService } from './services/create-user.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private createUserService: CreateUserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     return this.createUserService.execute(createUserDto);
