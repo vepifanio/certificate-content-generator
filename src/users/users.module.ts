812135e839773ba4391d495087b46880a6ca2f1b
vepifanio <vepifanio1@gmail.com>
2024-02-26 08:36:24 -0300

    feat: add create user service to users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FindUserByEmailService } from './services/find-user-by-email.service';
 import { PrismaModule } from 'src/prisma.module';
+import { CreateUserService } from './services/create-user.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [FindUserByEmailService],
-  exports: [FindUserByEmailService],
+  providers: [FindUserByEmailService, CreateUserService],
+  exports: [FindUserByEmailService, CreateUserService],
 })
 export class UsersModule {}
